8b6129a73795fbeccda0090f1932ab499e72c97c
puneet01994 <puneetchhapola@gmail.com>
2019-03-16 18:20:08 +0530

    passing dynamic arguments to the components.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi I am Puneet</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Puneet" age="24"/>
+        <Person name="Kapil" age="23">My Hobbies: Racing</Person>
+        <Person name="Meghali" age="25"/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', {}, 'Above written code will be compiled in this manner.'));
