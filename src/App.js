2e85516585b5c5e0938829f4b5c84dc41a776a1b
puneet01994 <puneetchhapola@gmail.com>
2019-03-20 23:59:43 +0530

    Changing the lists on some events.

--- src/App.js
+++ src/App.js
@@ -5,23 +5,33 @@ import Person from './Person/Person'
 class App extends Component {
   // Every time the state is changed, It will rerender the DOM/Update it.
   // Also, the state will be available only if we are using class based Components extends Component.
+  // Assigning some unique id with all the elements of the array. so that Virtual DOM can efficiently identify the changes.
+
   state = {
     person: [
-      {name: "Puneet", age: "24"},
-      {name: "Kapil", age: "23"},
-      {name: "Meghali", age: "25"}
+      {id:"as1", name: "Puneet", age: "24"},
+      {id:"as2", name: "Kapil", age: "23"},
+      {id:"as3", name: "Meghali", age: "25"}
     ],
     stateShowPerson: false
   };
 
-  nameChange = (event) => {
-    this.setState({
-      person: [
-        {name: "Puneet", age:"24"},
-        {name:  event.target.value, age:"23"},
-        {name: "Meghali", age: "25"}
-      ]
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.person.findIndex(p => {
+      return p.id === id;
     });
+
+    const persons = {
+      ...this.state.person[personIndex]
+    };
+
+    persons.name = event.target.value;
+
+    const person = [...this.state.person]
+
+    person[personIndex] = persons
+
+    this.setState({person: person});
   };
 
   togglePersonHandler = () => {
@@ -38,7 +48,7 @@ class App extends Component {
     const person = [...this.state.person]
     person.splice(index, 1);
     this.setState({person: person});
-  }
+  };
 
   render() {
 
@@ -58,6 +68,8 @@ class App extends Component {
         <div>
           {this.state.person.map((person, index) => {
             return <Person 
+            change={(event) => this.nameChangeHandler(event, person.id)}
+            key={person.id}
             name={person.name}
             age={person.age}
             // Any of the following 2 can b used.
